37c44ddd2c0932059cf9cd523c549da530810d63
Paul Tobin <tobster111@gmail.com>
2022-07-03 10:14:35 +0100

    find_by email and authenticate the password

--- sample_app/app/controllers/sessions_controller.rb
+++ sample_app/app/controllers/sessions_controller.rb
@@ -3,6 +3,11 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # Log the user in and redirect to the user's show page.
+    else
+      # Create an error message.
     render 'new', status: :unprocessable_entity
   end
 
