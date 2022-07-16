239f23fde8147c975632c7e0ee8504eacff1948b
Paul Tobin <tobster111@gmail.com>
2022-07-16 11:05:43 +0100

    Update

--- depot/test/models/product_test.rb
+++ depot/test/models/product_test.rb
@@ -7,7 +7,7 @@ class ProductTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
-  test "product attributes must not be empty" do
+  test 'product attributes must not be empty' do
     product = Product.new
     assert product.invalid?
     assert product.errors[:title].any?
@@ -16,18 +16,18 @@ class ProductTest < ActiveSupport::TestCase
     assert product.errors[:image_url].any?
   end
 
-  test "product price must be positive" do
-    product = Product.new(title: "My Book Title",
-                          description: "yyy",
-                          image_url: "zzz.jpg")
+  test 'product price must be positive' do
+    product = Product.new(title: 'My Book Title',
+                          description: 'yyy',
+                          image_url: 'zzz.jpg')
     product.price = -1
     assert product.invalid?
-    assert_equal ["must be greater than or equal to 0.01"],
+    assert_equal ['must be greater than or equal to 0.01'],
                  product.errors[:price]
 
     product.price = 0
     assert product.invalid?
-    assert_equal ["must be greater than or equal to 0.01"],
+    assert_equal ['must be greater than or equal to 0.01'],
                  product.errors[:price]
 
     product.price = 1
@@ -35,17 +35,16 @@ class ProductTest < ActiveSupport::TestCase
   end
 
   def new_product(image_url)
-    Product.new(title: "My Book Title",
-      description: "yyy",
-      price: 1,
-      image_url: image_url
-    )
+    Product.new(title: 'My Book Title',
+                description: 'yyy',
+                price: 1,
+                image_url:)
   end
 
-  test "image url" do
-    ok = %w{ fred.gif fred.jpg fred.png FRED.JPG FRED.Jpg
-              http://a.b.c/x/y/z/fred.gif }
-    bad = %w{ fred.doc fred.gif/more fred.gif.more }
+  test 'image url' do
+    ok = %w[ fred.gif fred.jpg fred.png FRED.JPG FRED.Jpg
+             http://a.b.c/x/y/z/fred.gif ]
+    bad = %w[fred.doc fred.gif/more fred.gif.more]
 
     ok.each do |name|
       assert new_product(name).valid?, "#{name} shouldn't be invalid"
@@ -56,12 +55,12 @@ class ProductTest < ActiveSupport::TestCase
     end
   end
 
-  test "product is not valid without a unique title" do
+  test 'product is not valid without a unique title' do
     product = Product.new(title: products(:ruby).title,
-                          description: "yyy",
+                          description: 'yyy',
                           price: 1,
-                          image_url: "fred.gif")
+                          image_url: 'fred.gif')
     assert product.invalid?
-    assert_equal ["has already been taken"], product.errors[:title]
+    assert_equal ['has already been taken'], product.errors[:title]
   end
 end
