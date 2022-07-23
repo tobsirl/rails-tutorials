32ea68583a19446ca07ce2cd6a544ce30baff884
Paul Tobin <tobster111@gmail.com>
2022-07-23 10:03:48 +0100

    Invalid cart

--- depot/app/controllers/carts_controller.rb
+++ depot/app/controllers/carts_controller.rb
@@ -67,4 +67,10 @@ class CartsController < ApplicationController
     def cart_params
       params.fetch(:cart, {})
     end
+
+    def invalid_cart
+      logger.error "Attempt to access invalid cart #{params[:id]}"
+      redirect_to store_url, notice: "Invalid cart"  
+    end
+    
 end
