5a85a5d7e10f29a37f6406f1f31c376e7fdf0218
Paul Tobin <tobster111@gmail.com>
2022-07-24 12:00:49 +0100

    Move the cart to the sidebar

--- depot/app/controllers/store_controller.rb
+++ depot/app/controllers/store_controller.rb
@@ -1,6 +1,6 @@
-# frozen_string_literal: true
-
 class StoreController < ApplicationController
+  include CurrentCart
+  before_action :set_cart
   def index
     @products = Product.order(:title)
   end
